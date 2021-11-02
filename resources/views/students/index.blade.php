5f0f986b291efe364548ddee38a69061b9ecd510
azriealarel <mr.azrieal@gmail.com>
2021-11-02 22:45:34 +0700

    Tambah relasi kelas-student

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -12,6 +12,13 @@
                                 {{ session('status') }} 
                             </div> 
                         @endif 
+
+                        <form class="form" method="get" action="{{ route('search') }}">
+                            <div class="form-group w-100 mb-3">
+                                <input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="search...">
+                                <button type="submit" class="btn btn-primary mb-1">Search</button>
+                            </div>
+                        </form>
                         
                         <a href="/students/create" class="btn btn-primary">Add Data</a> <br><br>
                         <table class="table table-responsive table-striped">
@@ -20,7 +27,6 @@
                                     <th>NIM</th> 
                                     <th>Name</th> 
                                     <th>Class</th> 
-                                    <th>Department</th> 
                                     <th>Action</th> 
                                 </tr> 
                             </thead> 
@@ -29,12 +35,11 @@
                                 <tr> 
                                     <td>{{ $s->nim }}</td> 
                                     <td>{{ $s->name }}</td> 
-                                    <td>{{ $s->class }}</td> 
-                                    <td>{{ $s->department }}</td> 
+                                    <td>{{ $s->kelas->class_name }}</td>
                                     <td>
                                         <form action="/students/{{$s->id}}" method="post">
                                             <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
-                                            <a href="/students/{{$s->id}}/" class="btn btn-warning">View</a>
+                                            <a href="/students/{{$s->id}}" class="btn btn-info">View</a>
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" name="delete" class="btn btn-danger">Delete</button>
